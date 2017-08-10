4810f10688af9f2e96e74e601b968a8dabfeb9c7
Josef Waller <josef@siriusapplications.com>
2017-08-10 11:56:17 -0700

    Add fetching pull requests within a certain period
    Add `GithubDashRepository.pull_requests`, which takes a certain number
    of days and returns all pull requests make within than nubmer of days.
    Ex: `GithubDashRepository.pull_requests(4)` would return all PRs made
    within 4 days. Limited to 100 PRs by default.

--- lib/github_dash/repository.rb
+++ lib/github_dash/repository.rb
@@ -1,4 +1,5 @@
 require "octokit"
+require 'pp'
 
 module GithubDash
   class Repository
@@ -10,6 +11,13 @@ module GithubDash
     # Get the raw octokit data
     def data
       @repo_data
+    end
+
+    # Get the pull requests opened in the last so many days
+    def get_pull_requests(days=7, up_to=100)
+      Octokit.pull_requests(@repo_data.full_name, :per_page => up_to).take_while do |pr|
+        pr.created_at.to_date > Date.today - days
+      end
     end
   end
 end
--- spec/github_dash_spec.rb
+++ spec/github_dash_spec.rb
@@ -11,4 +11,39 @@ RSpec.describe GithubDash do
       expect(repo.data.name).to eq("rails")
     end
   end
+
+  context "given a repository" do
+    before(:all) do
+      VCR.insert_cassette "rails", :record => :new_episodes
+      @repo = GithubDash::fetch_repository "rails/rails"
+    end
+    after(:all) do
+      VCR.eject_cassette "rails"
+    end
+    before(:each) do
+      # Stub the date, so that 'commits in the last n days' doesn't
+      #   rely on what day the test is being run
+      allow(Date).to receive(:today).and_return Date.new(2017, 8, 14)
+    end
+    it "gets all PRs in the last day" do
+      prs = @repo.get_pull_requests(days=1)
+      expect(prs.length).to eq(0)
+    end
+    it "gets all PRs in the last week" do
+      prs = @repo.get_pull_requests(days=7)
+      expect(prs.length).to eq(10)
+    end
+    it "gets all PRs in the last month" do
+      prs = @repo.get_pull_requests(days=30)
+      expect(prs.length).to eq(67)
+    end
+    it "limits the number of PRs to 100 by default" do
+      prs = @repo.get_pull_requests(days=1000)
+      expect(prs.length).to eq(100)
+    end
+    it "limits the number of PRs when given a limit" do
+      prs = @repo.get_pull_requests(days=100, up_to=25)
+      expect(prs.length).to eq(25)
+    end
+  end
 end
